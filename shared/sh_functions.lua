b7c3ae86b85a159b578e616bfdb898b3ff16bbd3
IO <48241519+IOxee@users.noreply.github.com>
2023-12-05 10:44:19 +0100

    Update sh_functions.lua

--- shared/sh_functions.lua
+++ shared/sh_functions.lua
@@ -1,20 +1,3 @@
--- lua function 
-function checkCapital()
-    if (Config.Framework == "esx") then
-        if (Config.Capital == 'money') then
-            return GetMoney()
-        elseif (Config.Capital == 'bank') then
-            return GetBankMoney()
-        end
-    elseif (Config.Framework == "qbcore") then
-        if (Config.Capital == 'money') then
-            return GetMoney()
-        elseif (Config.Capital == 'bank') then
-            return GetBankMoney()
-        end
-    end
-end
-
 function splitPrice(price)
     price = tostring(price)
     local formatted = price
